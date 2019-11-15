fa0c627c2bc17638e665bd7e2948c8ec2923271a
kingroon <psavchuk22@gmail.com>
2019-11-14 21:45:06 -0500

    Introduced the Basic Enemy

---
+++ Bullet-Hell-Game-Jam/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CircleCollider2D))]   
+public class BasicEnemy : MonoBehaviour
+{
+
+    public int size;
+
+    public float speed;
+
+    public enum Color
+    {
+        Red,
+        Yellow,
+        Blue
+    }
+
+    private GameObject player;
+    private Rigidbody2D rigidbody;
+    private CircleCollider2D collider;
+
+    private SpriteRenderer renderer;
+    private Transform childTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        collider = GetComponent<CircleCollider2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        renderer = GetComponentInChildren<SpriteRenderer>();
+        childTransform = transform.GetChild(0);
+
+        size = Random.Range(1, 10);
+
+        collider.radius = size * 0.25f;
+        childTransform.localScale = Vector2.one * size * 3f;
+
+        MoveTowardsPlayer();
+    }
+
+    void MoveTowardsPlayer()
+    {
+        Vector2 lookAtPoint = new Vector2(player.transform.position.x, player.transform.position.y);
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+
+        transform.up = lookAtPoint - currentPosition;
+
+        rigidbody.velocity = new Vector2(transform.up.x, transform.up.y) * speed;
+    }
+
+    void UpdateSize()
+    {
+        collider.radius = size * 0.25f;
+        childTransform.localScale = Vector2.one * size * 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //For when size changes in editor
+    private void OnValidate()
+    {
+        UpdateSize();
+    }
+}
