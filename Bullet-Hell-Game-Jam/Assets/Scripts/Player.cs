239a21c985c7fbdd5a7fc72b3bf407025f72b4b4
Kendall Adkins <adkins.kendall90@gmail.com>
2019-11-14 21:27:48 -0500

    Player Shooting

--- Bullet-Hell-Game-Jam/Assets/Scripts/Player.cs
+++ Bullet-Hell-Game-Jam/Assets/Scripts/Player.cs
@@ -9,13 +9,17 @@ public class Player : MonoBehaviour
     [Header("Player Movement")]
     [SerializeField] [Range(0, 10)] float movementSpeed = 6f;
     Vector2 _movement;
-    Vector3 mousePosition;
+    Vector3 _mousePosition;
 
     // TODO possible feature -- could add a dash or a blink (like Tracer) that slowly regenerates?! idk
 
     [Header("Player Health")]
     [SerializeField] [Range(0, 3)] int playerHealth = 1;
 
+    [Header("Player Shooting")]
+    [SerializeField] GameObject firePoint, projectile;
+
+    [Header("General")]
     public Rigidbody2D rb;
 
     void Start() {
@@ -28,26 +32,41 @@ public class Player : MonoBehaviour
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
 
-        _RotatePlayer();
+        // find mouse position
+        _mousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+
+        if (Input.GetButtonDown("Fire1")) { // using fire1 so that we have the flexibility to change the hotkey later
+            _Shoot();
+        }
     }
 
     void FixedUpdate() {
         // this will prevent from framerate variation
         _Move();
+        _RotatePlayer();
     }
 
     private void _Move() {
         Vector2 _velocity = _movement.normalized * movementSpeed;
-        rb.MovePosition(rb.position + _velocity *  Time.deltaTime);
+        rb.MovePosition(rb.position + _velocity * Time.deltaTime);
     }
 
     private void _RotatePlayer() {
-        // find mouse position
-        mousePosition = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         // find the amount of rotation needed to face mouse position
-        var angle = Mathf.Atan2(mousePosition.x, mousePosition.y) * Mathf.Rad2Deg;
+        var angle = Mathf.Atan2(_mousePosition.x, _mousePosition.y) * Mathf.Rad2Deg;
         // add amount needed to current rotation.
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
-        // @NOTE Not sure why I needed to use back instead of forward -- shrug
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.back); // @NOTE Not sure why I needed to use back instead of forward -- shrug
+    }
+
+    private void _Shoot() {
+        GameObject bullet = Instantiate(
+            projectile,
+            firePoint.transform.position,
+            Quaternion.identity
+        ) as GameObject;
+    }
+
+    public Vector3 GetMousePosition() {
+        return _mousePosition;
     }
 }
---
+++ Bullet-Hell-Game-Jam/Assets/Scripts/Projectile.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] [Range(0, 5)] float thrust = 2f;
+    public Rigidbody2D rb;
+
+    Vector3 _mousePosition;
+    Color color;
+
+    void Awake() {
+        _mousePosition = FindObjectOfType<Player>().GetMousePosition();        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _RotateBullet();
+        _AccelerateBullet();
+    }
+
+    private void _RotateBullet() {
+        // find the amount of rotation needed to face mouse position
+        var angle = Mathf.Atan2(_mousePosition.x, _mousePosition.y) * Mathf.Rad2Deg;
+        // add amount needed to current rotation
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.back);
+    }
+
+    private void _AccelerateBullet() {
+        float actualThrust = thrust * 10f;
+        rb.AddForce(transform.up * actualThrust, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        var tag = other.gameObject.tag;
+
+        if (other.gameObject.tag != "Player") { // ignore player
+            Debug.Log(tag);
+            if (tag == "Wall") {
+                Destroy(gameObject);
+            }
+
+            if (tag == "Enemy") {
+                // if correct color
+                    // deal dmg
+                // else
+                    // side effect
+            }
+        }
+    }
+}
